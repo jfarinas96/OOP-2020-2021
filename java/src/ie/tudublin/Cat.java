82db3a2494aeb547e9571e59125e68e8757ec4fa
Janae Fariñas <C19708735@mytudublin.ie>
2021-01-27 17:31:19 +0000

    killing the cat

--- java/src/ie/tudublin/Cat.java
+++ java/src/ie/tudublin/Cat.java
@@ -5,5 +5,31 @@ public class Cat extends Animal
     public Cat(String name)
     {
         super(name);
+        numLives = 9;
+    }
+
+    private int numLives;
+
+    public int getNumLives()
+    {
+        return numLives;
+    }
+
+    public void setNumLives(int numLives)
+    {
+        this.numLives = numLives;
+    }
+    public void kill()
+    {
+        if (numLives > 0)
+        {
+            numLives = numLives - 1;
+            System.out.println("Ouch");
+        }
+        
+        if (numLives == 0)
+        {
+            System.out.println("Dead");
+        }
     }
 }
