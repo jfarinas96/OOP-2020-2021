a8674aa34f0033f6e8b522cbbee3d4a4d8dd83eb
Janae Fariñas <C19708735@mytudublin.ie>
2021-02-15 13:18:54 +0000

    map function

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -4,8 +4,53 @@ import processing.core.PApplet;
 
 public class Arrays extends PApplet {
 
+    public float map1(float from, float start1, float stop1, float start2, float stop2)
+    {
+        /*
+        float range1 = stop1 - start1;
+        float range2 = stop2 - start2;
+        float howFar = from - start1;
+
+        return start2 + (howFar / range1) * range2;
+        */
+
+        float percent = (from - start1) / (stop1 - start1);
+        float add = (stop2 - start2) * percent;
+
+        return start2 + add;
+    }
+
+    public void drawGrid()
+    {
+        stroke(0, 255, 0);
+        float border = width * 0.1f;
+        textAlign(CENTER, CENTER);
+        for (int i = -5; i <= 5; i++)
+        {
+            float x = map1(i, -5, 5, border, width - border);
+            line(x, border, x, height - border);
+            line(border, x, width - border, x);
+            fill(255);
+            text(i, x, border * 0.5f);
+            text(i, border * 0.5f, x);
+        }
+    }
+
+
     public void settings() {
-        size(500, 500);                
+        size(500, 500);   
+
+        float f = map1(2, 0, 10, 0, width);
+        println(f); // should print 100
+
+        f = map1(9, 0, 1, 0, 10);
+        println(f); //should print 90
+
+        f = map1(250, 200, 300, 400, 500);
+        println(f); //should print 450
+
+        f = map1(5, 0, 10, 1000, 2000);
+        println(f); //should print 1500
     }
 
     int mode = 0;
@@ -18,13 +63,16 @@ public class Arrays extends PApplet {
     }
 
     public void setup() {
-        colorMode(HSB);
+        colorMode(RGB);
     }
 
     float offset = 0;
 
     public void draw() {
         background(0);
-        noStroke();
+        // drawGrid();
+        colorMode(HSB);
+        float c = map(mouseX, 0, width, 0, 255);
+        background(c, 255, 255);
     }
 }
--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -26,31 +26,26 @@ public class Loops extends PApplet {
         colorMode(HSB);
     }
 
-    float offset = 0;
-
     public void draw() {
         background(0);
-        noStroke();
         switch (mode)
         {
             case 0:
             {
-                float w = 200;
-                float h = 50;
-                rectMode(CENTER);                    
-                if (mouseX > cx -  (w /2) && mouseX < cx + (w/2) && mouseY > cy - (h/2) && mouseY < cy + (h/2))
+                fill(50, 255, 255);
+                if (mouseX < cx)
                 {
-                    fill(50, 255, 255);                
+                    rect(0, 0, cx, height);
                 }
                 else
                 {
-                    fill(200, 255, 255);
+                    rect(cx, 0, cx, height);
                 }
-                rect(cx, cy, w, h);
                 break;
-            }                
+            }
             case 1:
-                fill(50, 255, 255);                                    
+            {
+                fill(100, 255, 255);
                 if (mouseX < cx && mouseY < cy)
                 {
                     rect(0, 0, cx, cy);
@@ -68,12 +63,26 @@ public class Loops extends PApplet {
                     rect(cx, cy, cx, cy);
                 }
                 break;
+                /*
+                if (mouseX < cx + 100 && mouseX > cx - 100 && mouseY < cy + 50 && mouseY > cy - 50)
+                {
+                    fill(150, 255, 255);
+                }
+                else
+                {
+                    fill(200, 255, 255);
+                }
+                rect(cx - 100, cy - 50, 200, 100);
+                break;
+                */
+            }
             case 2:
             {
-                int numRects = (int)(mouseX / 10.0f);
+                noStroke();
+                int numRects = 10;
                 float w = width / (float) numRects;
                 float cgap = 255 / (float) numRects;
-                for(int i = 0 ; i < numRects ; i ++)
+                for (int i = 0; i < numRects; i++)
                 {
                     fill(i * cgap, 255, 255);
                     rect(i * w, 0, w, height);
@@ -82,88 +91,129 @@ public class Loops extends PApplet {
             }
             case 3:
             {
-                int numCircles = (int)(mouseX / 10.0f);
-                float w = width / (float) numCircles;
-                float cgap = 255 / (float) numCircles;
-                for(int i = 0 ; i < numCircles ; i ++)
+                int numRects = 10;
+                float w = width / (float) numRects;
+                float cgap = 255 / (float) numRects;
+                for (int i = 0; i < numRects; i++)
                 {
-                    fill(cgap * i, 255, 255);
-                    ellipse(w / 2 + (i * w), cy, w, w);
+                    fill(i * cgap, 255, 255);
+                    rect(i * w, i * w, w, w);
                 }
                 break;
+                /*
+                noStroke();
+                int numRects = 10;
+                float w = width / (float) numRects;
+                float cgap = 255 / (float) numRects;
+                int j = numRects - 1;
+                for (int i = 0; i < numRects; i++)
+                {
+                    fill(i * cgap, 255, 255);
+                    rect(i * w, i * w, w, w);
+                    rect(j * w, i * w, w, w);
+                    j--;
+                }
+                break;
+                */
             }
             case 4:
             {
-                int numLines = 5;
-                float theta = TWO_PI / (float) numLines;
-                float radius = 100;
-                for(int i = 0 ; i < numLines ; i ++)
+                noStroke();
+                int numCircles = 10;
+                float diameter = width;
+                float w = width / (float) numCircles;
+                float cgap = 220 / (float) numCircles;
+                int j = numCircles + 1;
+                for (int i = 0; i < numCircles; i++)
                 {
-                    float angle = theta * i;
-                    float x = sin(angle) * radius;
-                    float y = cos(angle) * radius;
-                    line(cx, cy, cx + x, cy + y);                    
-                }
-            }
-            break;
-            case 5:
-            {
-                rectMode(CORNER);
-                int numRects = (int) mouseX / 10;
-                float w = width / (float) numRects;
-                float cgap = 255 / (float) numRects;
-                for(int i = 0 ; i < numRects ; i ++)
-                {
-                    fill(cgap * i, 255, 255);
-                    rect(i * w, i * w, w, w);
-                    rect(width - ((i + 1) * w), i * w, w, w);
+                    fill(j * cgap, 255, 255);
+                    ellipse(cx, cy, diameter - (i * w), diameter - (i * w));
+                    j--;
                 }
                 break;
             }
-            case 6:
+            case 5:
             {
-                int numCircles = (int) mouseX / 10;
+                background(255);
+                int numCircles = 10;
+                float diameter = width / (float) numCircles;
                 float cgap = 255 / (float) numCircles;
-                float gap = width / (float) numCircles;
-                float w = width;
-                for(int i = numCircles ; i >= 1 ; i --)
+                for (int i = 0; i < numCircles; i++)
                 {
                     fill(i * cgap, 255, 255);
-                    w = i * gap;
-                    ellipse(cx, cy, w, w);
-                    
-                }                
+                    ellipse((i * diameter) + (diameter / 2), cy, diameter, diameter);
+                }
+                break;
+                /*
+                noStroke();
+                background(255);
+                int numCircles = 10;
+                float w = width / (float) numCircles;
+                float cgap = 100 / (float) numCircles;
+                for (int i = 0; i < numCircles; i++)
+                {
+                    for (int j = 0; j < numCircles; j++)
+                    {
+                        fill((i + j) * cgap, 255, 255);
+                        ellipse((i * w) + (w / 2), (j * w) + (w / 2), w, w);
+                    }
+                }
+                break;
+                */
+            }
+            case 6:
+            {
+                
             }
             case 7:
             {
-                offset += (mouseX / 100);
-                int numCircles = 20;
-                float w = width / (float)numCircles;
-                float cgap = 255 / (numCircles + numCircles);
-                for(int i = 0 ; i < numCircles ; i ++)
+                noStroke();
+                int numRects = 20;
+                float w = width / (float) numRects;
+                for (int i = 0; i < numRects; i++)
                 {
-                    for(int j = 0 ; j < numCircles ; j ++)
+                    for (int j = 0; j < numRects; j = j + 2)
                     {
-                        float c = ((cgap * (i + j)) + offset) % 255; 
-                        fill(c, 255, 255);
-                        ellipse((w / 2) + w * j, (w / 2) + w * i, w, w);
+                        fill(100, 255, 255);
+                        rect(j * w, i * w, w, w);
+                        fill(150, 255, 255);
+                        rect(j * w, (i + 1) * w, w, w);
                     }
                 }
             }
             case 8:
             {
-                int sides = (mouseX / 50);
+                /*
+                int sides = 5;
+                float theta = TWO_PI /(float) sides;
+                float radius = 100;
+                stroke(255);
+                for(int i = 0; i < sides; i++)
+                {
+                    float angle = theta * i;
+                    float x = sin(angle) * radius;
+                    float y = cos(angle) * radius;
+                    line(cx, cy, cx + x, cy - y);
+                }
+                break;
+                */
+                int sides = 5;
                 float theta = TWO_PI / (float) sides;
                 float radius = 200;
                 stroke(255);
-                for(int i = 1 ; i <= sides ; i ++)
+                for (int i = 1; i <= sides; i++)
                 {
                     float x1 = sin(theta * (i - 1)) * radius;
                     float y1 = cos(theta * (i - 1)) * radius;
                     float x2 = sin(theta * i) * radius;
                     float y2 = cos(theta * i) * radius;
-                    line(cx + x1, cy + y1, cx + x2, cy + y2);
+                    line(cx + x1, cy - y1, cx + x2, cy - y2);
                 }
+                break;
+            }
+            case 9:
+            {
+                //stars
             }
         }
     }
