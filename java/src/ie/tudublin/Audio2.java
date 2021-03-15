2a55167034f9a81ca825abba5d79a4bb487b1b5f
Janae Fariñas <C19708735@mytudublin.ie>
2021-03-15 13:56:46 +0000

    displaying frequencies as notes

--- java/src/ie/tudublin/Audio2.java
+++ java/src/ie/tudublin/Audio2.java
@@ -17,11 +17,29 @@ public class Audio2 extends PApplet {
 
     float y = 200;
     float lerpedY = y;
+    float lerpedAverage = 0;
 
     int which = 0;
 
     float[] bands;
     float[] smoothedBands;
+
+    private float[] frequencies = {293.66f, 329.63f, 369.99f, 392.00f, 440.00f, 493.88f, 554.37f, 587.33f
+        , 659.25f, 739.99f, 783.99f, 880.00f, 987.77f, 1108.73f, 1174.66f, 1318.51f, 1479.98f, 1567.98f, 1760.00f, 1975.53f, 2217.46f, 2349.32f};
+    String[] spellings = {"D,", "E,", "F,", "G,", "A,", "B,", "C", "D", "E", "F", "G", "A", "B","c", "d", "e", "f", "g", "a", "b", "c'", "d'", "e'", "f'", "g'", "a'", "b'", "c''", "d''"};
+
+    String spell(float freq) {
+        int note = 0;
+
+        for (int i = 0; i < frequencies.length - 1; i++) {
+            if (freq > frequencies[i] && freq < frequencies[i + 1]) {
+                note = i;
+                break;
+            }
+        }
+
+        return spellings[note];
+    }
 
     void calculateFrequencyBands() {
         for (int i = 0; i < bands.length; i++) {
@@ -51,7 +69,7 @@ public class Audio2 extends PApplet {
         colorMode(HSB);
         
         minim = new Minim(this);
-        ap = minim.loadFile("heroplanet.mp3", width);
+        ap = minim.loadFile("scale.wav", width);
         ai = minim.getLineIn(Minim.MONO, width, 44100, 16);
         ab = ap.mix;
 
@@ -78,9 +96,6 @@ public class Audio2 extends PApplet {
         }
     }
 
-    float lerpedAverage = 0;
-    int highestBand = 0;
-
     public void draw() {
         background(0);
         stroke(255);
@@ -94,10 +109,12 @@ public class Audio2 extends PApplet {
 
         fft.window(FFT.HAMMING);
         fft.forward(ab);
+
+        int highestBand = 0;
         
         for (int i = 0; i < fft.specSize(); i++) {
             stroke(map(i, 0, ab.size(), 0, 255), 255, 255);
-            // line(i, 0, i, fft.getBand(i) * halfHeight);
+            line(i, 0, i, fft.getBand(i) * halfHeight);
 
             if (fft.getBand(i) > fft.getBand(highestBand)) {
                 highestBand = i;
@@ -105,12 +122,13 @@ public class Audio2 extends PApplet {
         }
 
         float freq = fft.indexToFreq(highestBand);
-        stroke(255);
+        fill(255);
         text("Frequency: " + freq, width - 120, height - 15);
-        // text("Note: " + spell(freq), width - 120, height - 65);
+        text("Note: " + spell(freq), width - 120, height - 30);
 
         calculateFrequencyBands();
-
+        
+        /*
         float w = width / (float) bands.length;
         for(int i = 0 ; i < bands.length ; i ++)
         {
@@ -120,5 +138,6 @@ public class Audio2 extends PApplet {
             fill(c, 255, 255);
             rect(x, height, w, -smoothedBands[i]);
         }
+        */
     }
 }
